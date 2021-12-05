66b4ca32782205652a774cf7c825b19baa938526
Damien Alves <alves.d24@laposte.net>
2021-12-05 17:21:33 +0100

    refactor(src): change port

--- api/src/index.js
+++ api/src/index.js
@@ -3,7 +3,7 @@ import express from "express";
 import route from "./route/index.js";
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 route.preMiddleware(app, express);
 route.init(app);
