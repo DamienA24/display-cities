0fed138ff265e0d66ab9baee0e822f2f5b5560f9
Damien Alves <alves.d24@laposte.net>
2021-12-05 15:00:14 +0100

    refactor(cities): sort cities

--- api/src/module/cities/cities.js
+++ api/src/module/cities/cities.js
@@ -1,10 +1,29 @@
 import { selectCities } from "./dao";
 import { CodeValidator } from "./Validator/CitiesValidator";
 
+const postalCodeDom = ["971", "974", "977", "978", "988"];
+
 export const getCities = async (code) => {
   const Validator = new CodeValidator({ code });
   await Validator.validate();
 
-  const cities = await selectCities(code);
-  return cities;
+  const resultCities = await selectCities(code);
+  if (resultCities && resultCities.length > 0) {
+    const cities = {
+      metropole: resultCities.filter(
+        (city) => !postalCodeDom.includes(city.postal_code.slice(0, 3))
+      ),
+      dom: resultCities.filter((city) =>
+        postalCodeDom.includes(city.postal_code.slice(0, 3))
+      ),
+    };
+    return {
+      success: true,
+      data: cities,
+    };
+  }
+  return {
+    success: false,
+    data: {},
+  };
 };
--- api/src/module/cities/dao.js
+++ api/src/module/cities/dao.js
@@ -3,7 +3,7 @@ const Db = db.Db;
 
 export const selectCities = async (data) => {
   return await Db.query(
-    "SELECT * FROM city WHERE postal_code=:postal_code OR name=:name",
+    "SELECT * FROM city WHERE postal_code=:postal_code OR name=:name ORDER BY name ASC LIMIT 100",
     {
       postal_code: data,
       name: data,
